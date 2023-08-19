7935f069a50280b28f8018a602359766f76ae8f0
mbodaghabadi <142569752+mbodaghabadi@users.noreply.github.com>
2023-08-19 04:43:44 -0700

    Add files via upload

---
+++ 2/name.js
@@ -0,0 +1,21 @@
+let massge=" " ;
+const number = prompt('Enter number');
+for(let i=1; i<=number; i++){
+    if (i%2==0){
+        massge+=i+`
+        `;
+    }    
+    
+
+}
+console.log(`even number ${massge} `);
+
+
+
+
+
+
+
+
+
+
