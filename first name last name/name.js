7e0ce982f336c15311606b25e50356d60cf8d55b
mbodaghabadi <142569752+mbodaghabadi@users.noreply.github.com>
2023-08-20 12:24:47 -0700

    Add files via upload

---
+++ first name last name/name.js
@@ -0,0 +1,9 @@
+const first=prompt(`Enter your firstname`);
+const last=prompt(`enter yout last name`);
+
+let length1 = first.length;
+let length2 = last.length;
+
+console.log (first+last);
+console.log (length1+length2);
+
