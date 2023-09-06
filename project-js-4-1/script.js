1ad73c4f10876ebf9318eda214e7774cb0b3004f
mbodaghabadi <142569752+mbodaghabadi@users.noreply.github.com>
2023-09-06 07:16:56 -0700

    Add files via upload

---
+++ project-js-4-1/script.js
@@ -0,0 +1,44 @@
+const myName = []
+while (true) {
+    const names = prompt('please enter name');
+    myName.push(names);
+
+    if (names === 'stop') {
+        break;
+    }
+
+}
+myName.pop();
+console.log(myName);
+
+
+function joinTogether(joinNames) {
+    const length=joinNames.length
+
+
+    if(length === 0){
+        return 'nobody like this';
+     }
+
+     else if(length === 1){
+        return `${joinNames[0]} likes this.`
+     }
+     
+     
+
+    else if (length === 2) {
+        return `${names[0]} and ${names[1]} like this.`
+    }
+
+    else {
+            const lastTwoNames = joinNames.slice(-2).join(' and ');
+            const remainingNames = joinNames.slice(0, -2).join(', ');
+            return remainingNames + ', ' + lastTwoNames + ' like this';
+     
+        }
+
+
+}
+
+
+console.log(joinTogether(myName));
